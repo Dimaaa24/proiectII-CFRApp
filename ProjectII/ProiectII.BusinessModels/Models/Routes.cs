de4dae91a21d33df725d0dfda27df311b62d015e
Dimaaa24 <sergiu.gruia24@yahoo.com>
2024-05-22 17:30:16 +0300

    updated whole db and migrations project coupling

---
+++ ProjectII/ProiectII.BusinessModels/Models/Routes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProiectII.BusinessModels.Models
+{
+    public class Routes
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Source { get; set; } // where the train starts
+        public string Destination { get; set; }
+        public int TrainId { get; set; }
+        public Train Train { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+    }
+}
---
+++ ProjectII/ProiectII.WebAPI/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using ProjectII.DataAccess.Sqlite;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CFRContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
